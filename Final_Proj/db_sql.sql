881d50b8eb49f52d790d1d7e4e78974db4726412
zanbary34 <102601986+zanbary34@users.noreply.github.com>
2022-08-01 20:02:22 +0300

    new

--- Final_Proj/db_sql.sql
+++ Final_Proj/db_sql.sql
@@ -26,3 +26,26 @@ INSERT INTO team8.scooters (scooter_id, battery_level, longitude, latitude, firm
 INSERT INTO team8.scooters (scooter_id, battery_level, longitude, latitude, firm_name, helmet, city) VALUES (12, 0.8, 34.797, 31.259, 'lime', 1, 'Beer Sheva');
 INSERT INTO team8.scooters (scooter_id, battery_level, longitude, latitude, firm_name, helmet, city) VALUES (13, 0.7, 34.795, 31.2565, 'bird', 1, 'Beer Sheva');
 INSERT INTO team8.scooters (scooter_id, battery_level, longitude, latitude, firm_name, helmet, city) VALUES (14, 0.4, 34.795, 31.2565, 'wind', 1, 'Beer Sheva');
+
+create table parkings
+(
+    id        int auto_increment
+        primary key,
+    longitude float       null,
+    latitude  float       null,
+    city      varchar(20) null,
+    is_free   tinyint(1)  null,
+    constraint Parking_id_uindex
+        unique (id)
+);
+
+INSERT INTO team8.parkings (id, longitude, latitude, city, is_free) VALUES (1, 34.8296, 32.0971, 'Tel Aviv', 1);
+INSERT INTO team8.parkings (id, longitude, latitude, city, is_free) VALUES (2, 34.8275, 32.0979, 'Tel Aviv', 1);
+INSERT INTO team8.parkings (id, longitude, latitude, city, is_free) VALUES (3, 34.8337, 32.1, 'Tel Aviv', 1);
+INSERT INTO team8.parkings (id, longitude, latitude, city, is_free) VALUES (4, 34.7787, 32.0707, 'Tel Aviv', 1);
+INSERT INTO team8.parkings (id, longitude, latitude, city, is_free) VALUES (5, 34.7756, 32.0718, 'Tel Aviv', 1);
+INSERT INTO team8.parkings (id, longitude, latitude, city, is_free) VALUES (6, 34.7773, 32.0731, 'Tel Aviv', 1);
+INSERT INTO team8.parkings (id, longitude, latitude, city, is_free) VALUES (7, 34.7684, 31.2612, 'Beer Sheva', 1);
+INSERT INTO team8.parkings (id, longitude, latitude, city, is_free) VALUES (8, 34.7684, 31.2625, 'Beer Sheva', 1);
+INSERT INTO team8.parkings (id, longitude, latitude, city, is_free) VALUES (9, 34.7666, 31.264, 'Beer Sheva', 1);
+INSERT INTO team8.parkings (id, longitude, latitude, city, is_free) VALUES (10, 34.7644, 31.2655, 'Beer Sheva', 1);
